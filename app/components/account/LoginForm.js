6c5ba3dad2148831a6b383f0ecc825d65006ff69
Corina <corinacvp@gmail.com>
2020-06-30 10:26:11 -0400

    Saving the login form data in a state

--- app/components/account/LoginForm.js
+++ app/components/account/LoginForm.js
@@ -1,14 +1,47 @@
 import React, { useState } from "react";
 import { StyleSheet, View } from "react-native";
 import { Input, Icon, Button } from "react-native-elements";
+import { isEmpty } from "lodash";
+import { useNavigation } from "@react-navigation/native";
+import * as firebase from "firebase";
+import { validateEmail } from "../../utils/validations";
+import Loading from "../Loading";
 
 export default function LoginForm(props) {
   const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState(defaultFormValue());
+
+  const onChange = (e, type) => {
+    setFormData({ ...formData, [type]: e.nativeEvent.text });
+  };
+
+  const onSubmit = () => {
+    if (isEmpty(formData.email) || isEmpty(formData.password)) {
+      toastRef.current.show("Todos los campos son obligatorios");
+    } else if (!validateEmail(formData.email)) {
+      toastRef.current.show("El email no es correcto");
+    } else {
+      setLoading(true);
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(formData.email, formData.password)
+        .then(() => {
+          setLoading(false);
+          navigation.navigate("account");
+        })
+        .catch(() => {
+          setLoading(false);
+          toastRef.current.show("Email o contraseña incorrecta");
+        });
+    }
+  };
+
   return (
     <View style={styles.formContainer}>
       <Input
         placeholder="Correo electronico"
         containerStyle={styles.inputForm}
+        onChange={(e) => onChange(e, "email")}
         rightIcon={
           <Icon
             type="material-community"
@@ -22,6 +55,7 @@ export default function LoginForm(props) {
         containerStyle={styles.inputForm}
         password={true}
         secureTextEntry={showPassword ? false : true}
+        onChange={(e) => onChange(e, "password")}
         rightIcon={
           <Icon
             type="material-community"
@@ -35,9 +69,17 @@ export default function LoginForm(props) {
         title="Iniciar sesión"
         containerStyle={styles.btnContainerLogin}
         buttonStyle={styles.btnLogin}
+        onPress={onSubmit}
       />
     </View>
   );
+}
+
+function defaultFormValue() {
+  return {
+    email: "",
+    password: "",
+  };
 }
 
 const styles = StyleSheet.create({
