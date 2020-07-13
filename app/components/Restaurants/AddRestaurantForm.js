d704d1654526faebe8567b00449d0340b11a4c72
Corina Varas <corinacvp@gmail.com>
2020-07-13 10:24:42 -0400

    showing selected images of restaurants when you add a new one

--- app/components/Restaurants/AddRestaurantForm.js
+++ app/components/Restaurants/AddRestaurantForm.js
@@ -1,12 +1,17 @@
 import React, { useState } from "react";
 import { StyleSheet, View, ScrollView, Alert, Dimensions } from "react-native";
 import { Icon, Avatar, Image, Input, Button } from "react-native-elements";
+import * as Permissions from "expo-permissions";
+import * as ImagePicker from "expo-image-picker";
+import { map, size } from "lodash";
 
 export default function AddRestaurantForm(props) {
   const { toastRef, setIsLoading, navigation } = props;
   const [restaurantName, setRestaurantName] = useState("");
   const [restaurantAddress, setRestaurantAddress] = useState("");
   const [restaurantDescription, setRestaurantDescription] = useState("");
+  const [imagesSelected, setImagesSelected] = useState([]);
+  console.log(imagesSelected);
 
   const AddRestaurant = (e) => {
     console.log("OK");
@@ -21,6 +26,11 @@ export default function AddRestaurantForm(props) {
         setRestaurantName={setRestaurantName}
         setRestaurantAddress={setRestaurantAddress}
         setRestaurantDescription={setRestaurantDescription}
+      />
+      <UploadImage
+        toastRef={toastRef}
+        imagesSelected={imagesSelected}
+        setImagesSelected={setImagesSelected}
       />
       <Button
         title="Crear restaurant"
@@ -58,6 +68,56 @@ function FormAdd(props) {
     </View>
   );
 }
+function UploadImage(props) {
+  const { toastRef, setImagesSelected, imagesSelected } = props;
+
+  const imageSelect = async () => {
+    const resultPermissions = await Permissions.askAsync(
+      Permissions.CAMERA_ROLL
+    );
+
+    if (resultPermissions === "denied") {
+      toastRef.current.show(
+        "Es necesarios aceptar los permisos de la galeria, si los has rechazado tienes que ir a los ajustes y activarlos manualmente",
+        3000
+      );
+    } else {
+      const result = await ImagePicker.launchImageLibraryAsync({
+        allowsEditing: true,
+        aspect: [4, 3],
+      });
+
+      if (result.cancelled) {
+        toastRef.current.show(
+          "Has cerrado la galeria sin seleccionar ninguna imagen",
+          2000
+        );
+      } else {
+        setImagesSelected([...imagesSelected, result.uri]);
+      }
+    }
+  };
+  return (
+    <View style={styles.viewImage}>
+      {size(imagesSelected) < 4 && (
+        <Icon
+          type="material-community"
+          name="camera"
+          color="#7a7a7a"
+          containerStyle={styles.containerIcon}
+          onPress={imageSelect}
+        />
+      )}
+      {map(imagesSelected, (imageRestaurant, index) => (
+        <Avatar
+          key={index}
+          style={styles.miniatureStyle}
+          source={{ uri: imageRestaurant }}
+        />
+      ))}
+    </View>
+  );
+}
 
 const styles = StyleSheet.create({
   scrollView: {
@@ -79,5 +139,24 @@ const styles = StyleSheet.create({
   btnAddRestaurant: {
     backgroundColor: "#00a680",
     margin: 20,
+  },
+  viewImage: {
+    flexDirection: "row",
+    marginLeft: 20,
+    marginRight: 20,
+    marginTop: 30,
+  },
+  containerIcon: {
+    alignItems: "center",
+    justifyContent: "center",
+    marginRight: 10,
+    height: 70,
+    width: 70,
+    backgroundColor: "#e3e3e3",
+  },
+  miniatureStyle: {
+    width: 70,
+    height: 70,
+    marginRight: 10,
   },
 });
