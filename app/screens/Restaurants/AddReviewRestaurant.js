80eb8eaee81c9e4ec92534b9900c4d4f3835eba4
Corina Varas <corinacvp@gmail.com>
2020-07-19 18:38:32 -0400

    form to create comments

--- app/screens/Restaurants/AddReviewRestaurant.js
+++ app/screens/Restaurants/AddReviewRestaurant.js
@@ -1,15 +1,69 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text, StyleSheet } from "react-native";
+import { AirbnbRating, Button, Input } from "react-native-elements";
 
 export default function AddReviewRestaurant(props) {
   const { navigation, route } = props;
   const { idRestaurant } = route.params;
 
   return (
-    <View>
-      <Text>AddReviewRestaurant</Text>
+    <View style={styles.viewBody}>
+      <View style={styles.viewRating}>
+        <AirbnbRating
+          count={5}
+          reviews={["Pésimo", "Deficiente", "Normal", "Muy bueno", "Excelente"]}
+          defaultRating={0}
+          size={35}
+        />
+      </View>
+      <View style={styles.formReview}>
+        <Input placeholder="Titulo" containerStyle={styles.input} />
+        <Input
+          placeholder="Comentario..."
+          multiline={true}
+          inputContainerStyle={styles.textArea}
+        />
+        <Button
+          title="Enviar Comnetario"
+          containerStyle={styles.btnContainer}
+          buttonStyle={styles.btn}
+        />
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  viewBody: {
+    flex: 1,
+  },
+  viewRating: {
+    height: 110,
+    backgroundColor: "#f2f2f2",
+  },
+  formReview: {
+    flex: 1,
+    alignItems: "center",
+    margin: 10,
+    marginTop: 40,
+  },
+  input: {
+    marginBottom: 10,
+  },
+  textArea: {
+    height: 150,
+    width: "100%",
+    padding: 0,
+    margin: 0,
+  },
+  btnContainer: {
+    flex: 1,
+    justifyContent: "flex-end",
+    marginTop: 20,
+    marginBottom: 10,
+    width: "95%",
+  },
+  btn: {
+    backgroundColor: "#00a680",
+  },
+});
