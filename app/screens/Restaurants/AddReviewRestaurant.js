356458aaca31c70a03f08ca54fb8fc86e5863754
Corina Varas <corinacvp@gmail.com>
2020-07-31 13:18:28 -0400

    Saving rating and reviews in firestore

--- app/screens/Restaurants/AddReviewRestaurant.js
+++ app/screens/Restaurants/AddReviewRestaurant.js
@@ -1,6 +1,14 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useRef } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import { AirbnbRating, Button, Input } from "react-native-elements";
+import Toast from "react-native-easy-toast";
+import Loading from "../../components/loading";
+
+import { firebaseApp } from "../../utils/firebase";
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+const db = firebase.firestore(firebaseApp);
 
 export default function AddReviewRestaurant(props) {
   const { navigation, route } = props;
@@ -10,12 +18,38 @@ export default function AddReviewRestaurant(props) {
   const [title, setTitle] = useState("");
   const [review, setReview] = useState("");
   const [isLoading, setIsLoading] = useState(false);
+  const toasRef = useRef();
 
   // Función que va a enviar el comentario
   const addReview = () => {
-    console.log("rating", rating);
-    console.log("title", title);
-    console.log("review", review);
+    if (!rating) {
+      toasRef.current.show("No has dado ninguna puntuación");
+    } else if (!title) {
+      toasRef.current.show("El titulo es obligatorio");
+    } else if (!review) {
+      toasRef.current.show("El comentario es obligatorio");
+    } else {
+      setIsLoading(true);
+      const user = firebase.auth().currentUser;
+      const payload = {
+        idUser: user.uid,
+        avatarUser: user.photoURL,
+        idRestaurant: idRestaurant,
+        title: title,
+        rewiew: review,
+        rating: rating,
+        createAt: new Date(),
+      };
+      db.collection("reviews")
+        .add(payload)
+        .then(() => {
+          setIsLoading(false);
+        })
+        .catch(() => {
+          toasRef.current.show("Error al enviar el comentario");
+          setIsLoading(false);
+        });
+    }
   };
 
   return (
@@ -26,14 +60,20 @@ export default function AddReviewRestaurant(props) {
           reviews={["Pésimo", "Deficiente", "Normal", "Muy bueno", "Excelente"]}
           defaultRating={0}
           size={35}
+          onFinishRating={(value) => setRating(value)}
         />
       </View>
       <View style={styles.formReview}>
-        <Input placeholder="Titulo" containerStyle={styles.input} />
         <Input
-          placeholder="Comentario..."
+          placeholder="Titulo"
+          containerStyle={styles.input}
+          onChange={(e) => setTitle(e.nativeEvent.text)}
+        />
+        <Input
+          placeholder="Escribe tu comentario aquí"
           multiline={true}
           inputContainerStyle={styles.textArea}
+          onChange={(e) => setReview(e.nativeEvent.text)}
         />
         <Button
           title="Enviar Comentario"
@@ -41,6 +81,8 @@ export default function AddReviewRestaurant(props) {
           buttonStyle={styles.btn}
           onPress={addReview}
         />
+        <Toast ref={toasRef} position="center" opacity={0.9} />
+        <Loading isVisible={isLoading} text="Enviando comentario" />
       </View>
     </View>
   );
