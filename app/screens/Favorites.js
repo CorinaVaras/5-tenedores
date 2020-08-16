939581d5dc723ad950513f78d9f6e42b0efca24f
Corina Varas <corinacvp@gmail.com>
2020-08-15 23:03:01 -0400

    Calling list of favorite restaurants

--- app/screens/Favorites.js
+++ app/screens/Favorites.js
@@ -1,7 +1,65 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState, useRef, useCallback } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  ActivityIndicator,
+  TouchableOpacity,
+  Alert,
+} from "react-native";
+import { Image, Icon, Botton } from "react-native-elements";
+import { useFocusEffect } from "@react-navigation/native";
+
+import { firebaseApp } from "../utils/firebase";
+import firebase from "firebase";
+import "firebase/firestore";
+
+const db = firebase.firestore(firebaseApp);
 
 const Favorites = () => {
+  const [restaurants, setRestaurants] = useState(null);
+  const [userLogged, setUserLogged] = useState(false);
+
+  firebase.auth().onAuthStateChanged((user) => {
+    user ? setUserLogged(true) : setUserLogged(false);
+  });
+
+  useFocusEffect(
+    useCallback(() => {
+      if (userLogged) {
+        const idUser = firebase.auth().currentUser.uid;
+        db.collection("favorites")
+          .where("idUser", "==", idUser)
+          .get()
+          .then((response) => {
+            const idRestaurantsArray = [];
+            response.forEach((doc) => {
+              idRestaurantsArray.push(doc.data().idRestaurant);
+            });
+            getDataRestaurant(idRestaurantsArray).then((response) => {
+              const restuarants = [];
+              response.forEach((doc) => {
+                const restaurant = doc.data();
+                restaurant.id = doc.id;
+                restuarants.push(restaurant);
+              });
+              setRestaurants(restuarants);
+            });
+          });
+      }
+    }, [userLogged])
+  );
+
+  const getDataRestaurant = (idRestaurantsArray) => {
+    const arrayRestaurants = [];
+    idRestaurantsArray.forEach((idRestaurant) => {
+      const result = db.collection("restaurants").doc(idRestaurant).get();
+      arrayRestaurants.push(result);
+    });
+    return Promise.all(arrayRestaurants);
+  };
+
   return (
     <View>
       <Text> Favorites ... </Text>
