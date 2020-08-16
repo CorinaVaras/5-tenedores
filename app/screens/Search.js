e4578fa84da16004ad6b206e7aa216429ed500ef
Corina Varas <corinacvp@gmail.com>
2020-08-16 17:37:46 -0400

    Installing firesql to query Firestore with SQL syntax

--- app/screens/Search.js
+++ app/screens/Search.js
@@ -1,10 +1,25 @@
 import React, { useState, useEffect } from "react";
 import { StyleSheet, View, FlatList, Image } from "react-native";
 import { SearchBar, Icon, ListItem } from "react-native-elements";
+import { FireSQL } from "firesql";
+import firebase from "firebase/app";
+
+const fireSQL = new FireSQL(firebase.firestore(), { includeId: "id" });
 
 const Search = (props) => {
   const { navigation } = props;
   const [search, setSearch] = useState("");
+  const [restaurants, setRestaurants] = useState([]);
+
+  useEffect(() => {
+    if (search) {
+      fireSQL
+        .query(`SELECT * FROM restaurants WHERE name LIKE '${search}%'`)
+        .then((response) => {
+          setRestaurants(response);
+        });
+    }
+  }, [search]);
 
   return (
     <View>
